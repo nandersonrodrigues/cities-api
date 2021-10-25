84f9627ee36bdd7df6bb4f5d8f00ae866116c519
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-10-25 18:01:20 -0300

    Create State repository

---
+++ src/main/java/com/digitalinnovationone/citiesapi/staties/repositories/StateRepository.java
@@ -0,0 +1,7 @@
+package com.digitalinnovationone.citiesapi.staties.repositories;
+
+import com.digitalinnovationone.citiesapi.staties.entities.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
