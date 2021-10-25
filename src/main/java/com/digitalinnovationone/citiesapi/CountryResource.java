07562be1dee2118482c3c996ae6fbfbba638a0ab
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-10-25 16:13:53 -0300

    Add pageable support for list of countries

--- src/main/java/com/digitalinnovationone/citiesapi/CountryResource.java
+++ src/main/java/com/digitalinnovationone/citiesapi/CountryResource.java
@@ -2,11 +2,16 @@ package com.digitalinnovationone.citiesapi;
 
 import com.digitalinnovationone.citiesapi.countries.Country;
 import com.digitalinnovationone.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -19,8 +24,9 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries() {
-        return countryRepository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return countryRepository.findAll(page);
+    }
     }
 
 }
